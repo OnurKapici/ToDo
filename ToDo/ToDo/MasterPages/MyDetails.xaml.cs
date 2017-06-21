e774a9dc1f27d6265959369d10496a4a6895051c
Onur Kapıcı <onurkapici@gmail.com>
2017-06-21 12:17:48 +0300

    Create First

---
+++ ToDo/ToDo/MasterPages/MyDetails.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDo.MasterPages
+{
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyDetails : ContentPage
+    {
+
+        
+        public MyDetails()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void MenuButton_Clicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (((MyMasterPage)App.Current.MainPage).IsPresented == true)
+            {
+                ((MyMasterPage)App.Current.MainPage).IsPresented = false;
+            } else
+            {
+                ((MyMasterPage)App.Current.MainPage).IsPresented = true;
+            }
+        }
+    }
+
+    class MyDetailsViewModel : INotifyPropertyChanged
+    {
+
+        public MyDetailsViewModel()
+        {
+            IncreaseCountCommand = new Command(IncreaseCount);
+        }
+
+        int count;
+
+        string countDisplay = "You clicked 0 times.";
+        public string CountDisplay
+        {
+            get { return countDisplay; }
+            set { countDisplay = value; OnPropertyChanged(); }
+        }
+
+        public ICommand IncreaseCountCommand { get; }
+
+        void IncreaseCount() =>
+            CountDisplay = $"You clicked {++count} times";
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName]string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    }
+}
---
+++ ToDo/ToDo/MyMasterPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo.MasterPages;
+using Xamarin.Forms;
+
+namespace ToDo
+{
+    public class MyMasterPage:MasterDetailPage
+    {
+        public MyMasterPage()
+        {
+            Master = new MyMaster();
+            Detail = new MyDetails();
+            
+
+        }
+    }
+}
