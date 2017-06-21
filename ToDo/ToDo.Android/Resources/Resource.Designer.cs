4881be0b4c4ac06dd226b1b2fd8afc15893f715c
Murat Demirci <mdemirci@outlook.com>
2017-06-21 17:33:07 +0300

    button border problem fixed

--- ToDo/ToDo.Android/Resources/Resource.Designer.cs
+++ ToDo/ToDo.Android/Resources/Resource.Designer.cs
@@ -3085,8 +3085,8 @@ namespace ToDo.Droid
 			// aapt resource value: 0x7f0a015a
 			public const int Animation_Design_BottomSheetDialog = 2131362138;
 			
-			// aapt resource value: 0x7f0a0174
-			public const int AppCompatDialogStyle = 2131362164;
+			// aapt resource value: 0x7f0a0175
+			public const int AppCompatDialogStyle = 2131362165;
 			
 			// aapt resource value: 0x7f0a00a5
 			public const int Base_AlertDialog_AppCompat = 2131361957;
@@ -3534,6 +3534,9 @@ namespace ToDo.Droid
 			
 			// aapt resource value: 0x7f0a0173
 			public const int MainTheme_Base = 2131362163;
+			
+			// aapt resource value: 0x7f0a0174
+			public const int NoShadowButton = 2131362164;
 			
 			// aapt resource value: 0x7f0a0032
 			public const int Platform_AppCompat = 2131361842;
